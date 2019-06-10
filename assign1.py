5ca17ffb02a4c6f3c69fa315452827b2f90afe0f
Mithusri9 <dmithusri9it@gamil.com>
2019-06-11 05:24:19 +0530

    assign1

---
+++ assign1.py
@@ -0,0 +1,10 @@
+population={"shanghai":17.8,"istanbul":13.3,"karachi":13.0,"mumbai":12.5}
+print(population)
+animals = {'dogs': [20, 10, 15, 8, 32, 15], 'cats': [3,4,2,8,2,4],
+           'rabbits': [2, 3, 3], 'fish': [0.3, 0.5, 0.8, 0.3, 1]}
+print(animals['dogs'])
+print(animals['dogs'][3])
+print(animals['fish'])
+a=[1,2,2,3,3,3,4,4,4,4]
+b=set(a)
+print(len(a)-len(b))
