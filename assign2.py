86eef9835e016d4b050850aec7b8d34b04301265
Mithusri9 <dmithusri9it@gamil.com>
2019-06-11 15:06:21 +0530

    assign2

--- assign2.py
+++ assign2.py
@@ -6,3 +6,22 @@ elif answer>guess:
     print("guess is too low")
 else:
     print("guess is equal to answer")
+    
+    
+    
+    
+    
+for j in range(30):
+    if j%5==0:
+        print(j)
+        
+        
+        
+        
+int_lim=577
+i=1
+while((i*i)<int_lim):
+    near_sq=i*i
+    i=i+1
+print(near_sq)
+        
